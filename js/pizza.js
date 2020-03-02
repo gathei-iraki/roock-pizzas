11e992c1b0562a9b985bb2716ee3ec2dbbf7cf59
gathei-iraki <slyiraki@gmail.com>
2020-03-02 08:53:55 +0300

    Added js folder to repository

---
+++ js/pizza.js
@@ -0,0 +1,51 @@
+$().ready(function () {
+    $("#vegeterian-image").click(function () {
+      $("#vegeterian-whole").toggle();
+      $("#vegeterian-text").toggle();
+    });
+
+    $("#hawaian-pic").click(function () {
+      $("#hawaian-whole").toggle();
+      $("#hawaian-text").toggle();
+    });
+
+    $("#chicken-image").click(function () {
+      $("#chicken-whole").toggle();
+      $("#chicken-text").toggle();
+    });
+  });
+
+
+  $("#vegeterian-image").click(function () {
+    $("#vegeterian-whole").toggle();
+    $("#vegeterian-text").toggle();
+  });
+
+  $("#hawaian-pic").click(function () {
+    $("#hawaian-whole").toggle();
+    $("#hawaian-text").toggle();
+  });
+
+  $("#chicken-image").click(function () {
+    $("#chicken-whole").toggle();
+    $("#chicken-text").toggle();
+  });
+
+
+
+
+$("#vegeterian-image").click(function () {
+    $("#vegeterian-whole").toggle();
+    $("#vegeterian-text").toggle();
+  });
+
+  $("#hawaian-pic").click(function () {
+    $("#hawaian-whole").toggle();
+    $("#hawaian-text").toggle();
+  });
+
+  $("#chicken-image").click(function () {
+    $("#chicken-whole").toggle();
+    $("#chicken-text").toggle();
+  });
+
